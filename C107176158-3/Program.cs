e1f8f4f0481d8a27c57bb33637c653a4e58cd121
user <user@A401-PC43>
2022-03-23 17:11:20 +0800

    加入專案檔案。

---
+++ C107176158-3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C107176158_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            string symbols = "0123456789ABCDEF";
+            num = Convert.ToInt32(Console.ReadLine());
+            string result = "";
+            while (num > 16)
+            {
+                result = symbols[num % 16].ToString() + result;
+                num = (num / 16);
+            }
+            result =  symbols[num]+result
+                ;
+            Console.WriteLine("binary number is {0}", result);
+            Console.ReadLine();
+               
+        }
+    }
+}
